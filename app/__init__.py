b9367c706882ee5381a483306752c2a4c2ad52cb
nikkis <nikkisharpley@gmail.com>
2018-11-07 15:01:13 -0500

    Password reset Password reset with email authentication token.

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@ import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
-
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models
 from app import routes, models, errors
