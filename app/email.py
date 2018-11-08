d8b78b78edfc8cceeeefd773e50793b29a707f55
nikkis <nikkisharpley@gmail.com>
2018-11-08 17:45:35 -0500

    Styling Added Bootstrap for styling and form simplification.

--- app/email.py
+++ app/email.py
@@ -1,8 +1,8 @@
+from threading import Thread
 from flask_mail import Message
 from app import mail
 from flask import render_template
 from app import app
-from threading import Thread
 
 def send_async_email(app, msg):
     with app.app_context():
